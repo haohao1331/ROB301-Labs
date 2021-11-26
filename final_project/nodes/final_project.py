a9e65de07d50e085ec2608c74242a8aa1a060485
Leo <haohao1331@gmail.com>
2021-11-26 11:46:27 -0500

    milestone 3 baysian loc

--- final_project/nodes/final_project.py
+++ final_project/nodes/final_project.py
@@ -9,20 +9,34 @@ import numpy as np
 import colorsys
 from numpy import dot
 from numpy.linalg import norm
+from datetime import datetime
+np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+(b, g, y, r) = (2, 1, 3, 0)
+colors = {2: "blue", 1: 'green', 3: 'yellow', 0: 'red'}
+state_color_map = np.array([y, r, g, b, b, r, g, b, y, r, g])
 
+colour_codes = np.array([
+    np.array([186.8, 115.2, 133.4]),  # red
+    np.array([120.4, 165.6, 129.6]),  # green
+    np.array([115.25, 133.25, 173.25]),  # blue
+    np.array([203.8, 191.8, 126.6]),  # yellow
+])
+
+print(colour_codes.shape)
+
+def normalize(a):
+    return a / np.sum(a)
 
 class BayesLoc:
-    def __init__(self, p0, colour_codes, colour_map):
+    def __init__(self):
         self.colour_sub = rospy.Subscriber(
             "mean_img_rgb", UInt32MultiArray, self.colour_callback
         )
         self.line_sub = rospy.Subscriber("line_idx", String, self.line_callback)
         self.cmd_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
 
-        self.num_states = len(p0)
-        self.colour_codes = colour_codes
-        self.colour_map = colour_map
-        self.probability = p0
+        self.num_states = len(state_color_map)
+        self.probability = normalize(np.ones(self.num_states))
         self.state_prediction = np.zeros(self.num_states)
 
         self.cur_colour = None  # most recent measured colour
@@ -31,23 +45,24 @@ class BayesLoc:
         self.twist = Twist()
         self.actual = 320
         self.desired = 320
-        self.v = 0.1
+        self.v = 0.07
         self.k = 0.2
         self.kp = 0.004  # 0.025 -> perfect oscillation
-        self.ki = 0.00000
+        self.ki = 0.00001
         self.kd = 0.001
         self.integral = 0
         self.derivative = 0
         self.maxIntegral = 1000
         self.lasterror = 0
         self.on_color = False
-        self.on_color_time = 32
+        self.on_color_time = 43
         self.on_color_count = self.on_color_time
 
         # color testing
         self.colors = []
-        self.log = open('log.txt', 'w')
-
+        self.now = datetime.now()
+        current_time = self.now.strftime("%H_%M_%S")
+        self.log = open('test_camera/' + current_time + '.txt', 'w+')
 
 
     def colour_callback(self, msg):
@@ -56,7 +71,7 @@ class BayesLoc:
         """
         self.cur_colour = np.array(msg.data)  # [r, g, b]
         # print("cur color: ", self.cur_colour)
-        self.colors.append(self.cur_colour)
+        # self.colors.append(self.cur_colour)
 
     def PID(self):
         error = float(self.desired - self.actual)
@@ -66,22 +81,25 @@ class BayesLoc:
             self.integral = 0
         self.integral = np.sign(self.integral) * min(abs(self.integral), self.maxIntegral)
         self.derivative = error - self.lasterror
-        if False:
-            print("error: " + str(error))
-            print("last error: " + str(self.lasterror))
-            print("integral: ", self.integral)
-            print("derivative: ", self.derivative)
-            print('\n\n\n\n\n\n')
         self.twist.linear.x = self.v
         correction = self.kp * error + self.ki * self.integral + self.kd * self.derivative
         # correction = 0
         line_color = np.array([160, 160, 160])
         if self.cur_colour != None and not self.on_color:
             diff = dot(self.cur_colour, line_color) / (norm(self.cur_colour) * norm(line_color))
-            print(diff)
+            # print(diff)
             if diff < 0.99:
                 self.on_color = True
-                
+                # print(self.cur_colour)
+                dot_products = [False] * len(colour_codes)
+                for i in range(len(colour_codes)):
+                    dot_products[i] = dot(self.cur_colour, colour_codes[i]) / (norm(colour_codes[i]) * norm(self.cur_colour))
+                # print(dot_products)
+                index = dot_products.index(max(dot_products))
+                print("measured color: ", colors[index])
+                self.colors.append(str(self.cur_colour) + "  " + str(diff) + "   " + str(dot_products) + " " + colors[dot_products.index(max(dot_products))])
+                self.state_predict()
+                self.state_update(index)
         
         if self.on_color:
             correction = 0
@@ -89,14 +107,22 @@ class BayesLoc:
             if self.on_color_count == 0:
                 self.on_color_count = self.on_color_time
                 self.on_color = False
+            
+            # do the baysian thing
         
-        print(self.on_color)
-        print(self.on_color_count)
-        print("\n")
+        if False:
+            print("error: " + str(error))
+            print("last error: " + str(self.lasterror))
+            print("integral: ", self.integral)
+            print("derivative: ", self.derivative)
+            print("correction: ", correction)
+            print('\n\n\n\n\n')
+        # print(self.on_color)
+        # print(self.on_color_count)
+        # print("\n")
 
         self.twist.angular.z = correction
         self.lasterror = error
-
 
 
     def follow_the_line(self):
@@ -116,77 +142,42 @@ class BayesLoc:
         while not rospy.is_shutdown() and self.cur_colour is None:
             rate.sleep()
 
-    def state_model(self, u):
-        """
-        State model: p(x_{k+1} | x_k, u)
-
-        TODO: complete this function
-        """
-
-    def measurement_model(self, x):
-        """
-        Measurement model p(z_k | x_k = colour) - given the pixel intensity,
-        what's the probability that of each possible colour z_k being observed?
-        """
-        if self.cur_colour is None:
-            self.wait_for_colour()
-
-        # prob = np.zeros(len(colourCodes))
-
-        """
-        TODO: You need to compute the probability of states. You should return a 1x5 np.array
-        Hint: find the euclidean distance between the measured RGB values (self.cur_colour)
-            and the reference RGB values of each colour (self.ColourCodes).
-        """
-
-        # return prob
-
     def state_predict(self):
         rospy.loginfo("predicting state")
-        """
-        TODO: Complete the state prediction function: update
-        self.state_prediction with the predicted probability of being at each
-        state (office)
-        """
+        new = np.copy(self.probability)
+        for i in range(len(new)):
+            self.probability[i] = new[(i-1) % self.num_states]
+        print("apriori update: ", self.probability)
 
-    def state_update(self):
+    def state_update(self, measurement):
         rospy.loginfo("updating state")
-        """
-        TODO: Complete the state update function: update self.probabilities
-        with the probability of being at each state
-        """
+        new_prob = np.zeros(self.num_states)
+        for j in range(len(self.probability)):
+            if state_color_map[j] == measurement:
+                prob = 0.85
+            else:
+                prob = 0.05
+            new_prob[j] = self.probability[j] * prob
+        
+        self.probability = normalize(new_prob)
+        print("posterior update: ", self.probability)
 
     def done(self):
         for s in self.colors:
-            # self.log.write(str(s) + "\n")
-            pass
+            self.log.write(str(s) + "\n")
+            # pass
         self.log.close()
 
 
 if __name__ == "__main__":
-
-    # This is the known map of offices by colour
-    # 0: red, 1: green, 2: blue, 3: yellow, 4: line
-    # current map starting at cell #2 and ending at cell #12
-    colour_map = [3, 0, 1, 2, 2, 0, 1, 2, 3, 0, 1]
 
     # TODO calibrate these RGB values to recognize when you see a colour
     # NOTE: you may find it easier to compare colour readings using a different
     # colour system, such as HSV (hue, saturation, value). To convert RGB to
     # HSV, use:
     # h, s, v = colorsys.rgb_to_hsv(r / 255.0, g / 255.0, b / 255.0)
-    colour_codes = [
-        [167, 146, 158],  # red
-        [163, 184, 100],  # green
-        [173, 166, 171],  # blue
-        [167, 170, 117],  # yellow
-        [150, 150, 150],  # line
-    ]
 
-    # initial probability of being at a given office is uniform
-    p0 = np.ones_like(colour_map) / len(colour_map)
-
-    localizer = BayesLoc(p0, colour_codes, colour_map)
+    localizer = BayesLoc()
 
     rospy.init_node("final_project")
     rospy.sleep(0.5)
